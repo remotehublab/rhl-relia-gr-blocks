95612886ec0831139f22a55daf9115b694ea87d6
Pablo Orduña <pablo@labsland.com>
2022-10-28 16:14:09 -0700

    Implement pushbutton

--- python/relia_blocks/push_button.py
+++ python/relia_blocks/push_button.py
@@ -1,31 +1,36 @@
 from relia_blocks.api import downloader, VariableBlock
 
 class PushButtonWidget(VariableBlock):
-    def __init__(self, identifier, choices):
-        self.choices = choices
-        self.state = 0
-        self._pushbutton_callback = None
+    def __init__(self, identifier, choices, label):
+        self.label = label
+        self.state = False
+        self._push_button_pressed_callback = None
+        self._push_button_released_callback = None
         super().__init__(identifier=identifier, callback=self.on_new_data)
 
     def pressed(self, callback):
-        self.state = 1
-        self._pushbutton_callback = callback
+        self._push_button_pressed_callback = callback
 
     def get_data(self) -> dict:
         return {
             'params': {
                 'state': self.state,
-                'choices': self.choices,
+                'label': self.label,
             }
         }
 
     def released(self, callback):
-        self._pushbutton_callback = callback
-        self.state = 0
+        self._push_button_released_callback = callback
 
     def on_new_data(self, data_item):
-        #new_state = data_item['value']
-        #self.state = new_state
-        new_state_value = self.choices.get(self.state)
-        if self._pushbutton_callback is not None and new_state_value is not None:
-            self._pushbutton_callback(new_state_value)
+        print("new data on push button:", data_item)
+        new_state = data_item['value']
+        self.state = new_state
+
+        if new_state == True:
+            if self._push_button_pressed_callback is not None:
+                self._push_button_pressed_callback()
+        elif new_state == False:
+            if self._push_button_released_callback is not None:
+                self._push_button_released_callback()
+
